9efaf42de64c263fd79a72decb6f10423f7e834b
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-08 12:54:27 -0500

    changed syntax on ShoppingCartItem component

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Item = props => {
+  const { image, title, price } = props.item;
+
   return (
     <div className="shopping-cart_item">
-      <img src={props.item.image} alt={`${props.item.title} book`} />
+      <img src={image} alt={`${title} book`} />
 
       <div>
-        <h1>{props.item.title}</h1>
-        <p>$ {props.item.price}</p>
+        <h1>{title}</h1>
+        <p>$ {price}</p>
         <button onClick={() => props.removeItem(props.item)}>
           Remove from cart
         </button>
